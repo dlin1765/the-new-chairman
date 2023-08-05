5e90b8be3d48a119d2dbb1fc459009c2ff80447e
dlin1765 <dlin@scu.edu>
2023-08-04 17:37:30 -0700

    enemy AI updates
    Enemy now plays the correct suit and can check if it has a card of the same type to play as well

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -10,16 +10,18 @@ public class GameControl : MonoBehaviour
     public GameObject playerHandCopy;
     public GameObject enemyHandCopy;
     public GameObject deckHandCopy;
-    public GameObject deckCopy;
+  
     public GameObject cardCopy;
     public GameObject tableTop;
     public Sprite[] spriteArray;
+    
     public GameState state;
     public bool cardPlayed = false;
     PlayerHandController p1;
     EnemyHandController e1;
     ButtController d1;
     TableHandController t1;
+    card c1;
     public int cardTracker;
     public int turnOrder;
     // Start is called before the first frame update
@@ -49,6 +51,7 @@ public class GameControl : MonoBehaviour
         e1 = enemyHandCopy.GetComponent<EnemyHandController>();
         d1 = deckHandCopy.GetComponent<ButtController>();
         t1 = tableTop.GetComponent<TableHandController>();
+        c1 = cardCopy.GetComponent<card>();
         state = GameState.ENEMYTURN;
         StartCoroutine(enemyTurn());
         //p1.printDeck();
@@ -60,41 +63,91 @@ public class GameControl : MonoBehaviour
         // implement some kind of coroutine to make it so it pauses between turns 
         //e1.printDeck();
         // start another coroutine that checks if the player has placed a card on the table top 
+        int lowBound = 0;
+        bool found = false;
+        int highBound = 0;
         yield return new WaitForSeconds(2f);
         int pickOrder = 0; 
-        if (t1.tablesHand.Count != 0)
+        if (t1.tablesHand.Count != 0) // if the table has cards on it, check for the suit so it can play the correct card
         {
             // implement the rules here 
-            pickOrder = 0;      
+            int tableNum = t1.tablesHand[t1.tablesHand.Count-1];
+            // tableNum = 3 
+   
+            if(tableNum >= 0 && tableNum <= 12)
+            {
+                lowBound = 0;
+                highBound = 12;
+            }
+            else if(tableNum > 12 && tableNum <= 25)
+            {
+                lowBound = 13;
+                highBound = 25;
+            }
+            else if(tableNum > 25 && tableNum <= 38)
+            {
+                lowBound = 25;
+                highBound = 38;
+            }
+            else
+            {
+                lowBound = 38;
+                highBound = 51;
+            }
+            
+            for(int i = 0; i < e1.enemyHand.Count; i++)
+            {
+                if ((e1.enemyHand[i] >= lowBound && e1.enemyHand[i] <= highBound) || (t1.tablesHand[t1.tablesHand.Count-1] % 13 == e1.enemyHand[i] % 13))
+                {
+                    pickOrder = i;
+                    found = true;
+                    break;
+                }
+            }
         }
-        GameObject copy = GameObject.Find("ButtonController");
-        GameObject cardTemp = GameObject.Find("card");
-        //ButtController copy1 = copy.GetComponent<ButtController>();
-        var tableTransform = t1.transform;
-        var enemyTransform = e1.transform;
-        int temp = t1.tablesHand.Count;
-        t1.tablesHand.Add(e1.enemyHand[pickOrder]);
-        //enemyTransform.GetChild(pickOrder).gameObject.GetComponent<card>().spriteRenderer.sprite = enemyTransform.GetChild(pickOrder).gameObject.GetComponent<card>().spriteArray[e1.enemyHand[pickOrder]];
-        enemyTransform.GetChild(pickOrder).gameObject.GetComponent<SpriteRenderer>().sprite = spriteArray[e1.enemyHand[pickOrder]];
-        enemyTransform.GetChild(pickOrder).gameObject.transform.SetParent(tableTransform, false);
-        e1.enemyHand.RemoveAt(pickOrder);
+        else // if there are no cards on the table, just play the first card in the hand
+        {
+            pickOrder = 0;
+            found = true;
+        }
+        if (found == false) // if the ai does not have a card it can play, draw a card from the deck
+        {
+            GameObject copy = GameObject.Find("ButtonController");
+            c1.SpawnCard(e1.gameObject, copy.GetComponent<ButtController>().deckList[0], false, 1);
+            copy.GetComponent<ButtController>().deckList.RemoveAt(0);
+        }
+        else // if the AI has a card it can play, move the card from the hand to the table 
+        {
+            GameObject copy = GameObject.Find("ButtonController");
+            GameObject cardTemp = GameObject.Find("card");
+            //ButtController copy1 = copy.GetComponent<ButtController>();
+            var tableTransform = t1.transform;
+            var enemyTransform = e1.transform;
+            int temp = t1.tablesHand.Count;
+            t1.tablesHand.Add(e1.enemyHand[pickOrder]);
+            //enemyTransform.GetChild(pickOrder).gameObject.GetComponent<card>().spriteRenderer.sprite = enemyTransform.GetChild(pickOrder).gameObject.GetComponent<card>().spriteArray[e1.enemyHand[pickOrder]];
+            enemyTransform.GetChild(pickOrder).gameObject.GetComponent<SpriteRenderer>().sprite = spriteArray[e1.enemyHand[pickOrder]];
+            enemyTransform.GetChild(pickOrder).gameObject.transform.SetParent(tableTransform, false);
+            e1.enemyHand.RemoveAt(pickOrder);
 
+            
+        }
         // remember to add functionality to skip turns with aces by upping turn order by 2
         turnOrder++;
         // have to change it to player turn
-        if(turnOrder == 1)
+        if (turnOrder == 1)
         {
 
         }
-        else if(turnOrder == 2)
+        else if (turnOrder == 2)
         {
 
         }
-        else if(turnOrder == 2)
+        else if (turnOrder == 2)
         {
 
         }
-        else if(turnOrder == 3)
+        else if (turnOrder == 3)
         {
 
         }
@@ -109,7 +162,7 @@ public class GameControl : MonoBehaviour
         // if statement to change the state to the correct one 
 
     }
-    
+
     IEnumerator playerTurn()
     {
         yield return new WaitUntil(() => playedCard());
